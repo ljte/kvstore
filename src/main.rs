4247e5147f06567aea8632aa963ae2d24adec6df
ljte <ljte823@gmail.com>
2021-06-23 22:18:15 +0300

    broken

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::collections::HashMap;
+use std::{env, process::exit};
+
+enum KVAction {
+    SET,
+    GET,
+}
+
+struct UnknownAction;
+
+impl From<&str> for KVAction {
+    fn from(s: &str) -> Result<Self, UnknownAction> {
+        match s.to_lowercase().as_ref() {
+            "get" => Ok(Self::GET),
+            "set" => Ok(Self::SET),
+            _ => Err(UnknownAction),
+        }
+    }
+}
+
+fn parse_args() -> (String, String) {
+    let mut args = env::args().skip(1);
+
+    if args.len() < 3 {
+        eprintln!("Not enough arguments");
+        exit(1);
+    }
+
+    let action = 
+    let key = args.next().unwrap();
+    let value = args.next().unwrap();
+
+    (key, value)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (key, value) = parse_args();
+    let mut kvstore = HashMap::kvstore.insert(key, value);
+    println!("{:?}", kvstore);
 }
